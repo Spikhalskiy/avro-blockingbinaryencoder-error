dc496f4b038fd9fffdc3d81db079aff6985283c4
Dmitry Spikhalskiy <dmitry@spikhalskiy.com>
2015-10-04 17:48:39 +0300

    cleanup

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,7 +1,6 @@
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.nio.ByteBuffer;
 
 import org.apache.avro.io.*;
@@ -17,6 +16,8 @@ import static junit.framework.Assert.assertEquals;
  * @author Dmitry Spikhalskiy <dspikhalskiy@pulsepoint.com>
  */
 public class Tests {
+    //In this test first payload will be written and type only after it, payload contains series of 0001, as a result, enum will be read as 0 - Type.ZERO
+    //By replacing putInt(1) with putInt(78789797) we can make ArrayIndexOutOfBoundsException
     @Test
     public void failingTest() throws IOException {
         //construct entity
@@ -50,6 +51,49 @@ public class Tests {
         BinaryDecoder avroDecoder = DecoderFactory.get().binaryDecoder(inputStream, null);
         Entity deserailized = datumReader.read(null, avroDecoder);
 
+        //check
+        assertEquals(entity.getEtype(), deserailized.getEtype());
+        assertEquals(entity.getPayload(), deserailized.getPayload());
+    }
+
+    //Here we make more obvious that enum is read from payload -
+    //java.lang.ArrayIndexOutOfBoundsException: 2
+    //at org.apache.avro.io.ResolvingDecoder.readEnum(ResolvingDecoder.java:257)
+    //as a result
+    @Test
+    public void anotherFailingTest() throws IOException {
+        //construct entity
+
+        // BufferedBinaryEncoder#159 (!bytes.hasArray() && bytes.remaining() > bulkLimit) - here we make "!bytes.hasArray()"
+        ByteBuffer payload = ByteBuffer.allocateDirect(8 * 1024);
+
+        // BufferedBinaryEncoder#159 (!bytes.hasArray() && bytes.remaining() > bulkLimit) - here we make "bytes.remaining() > bulkLimit"
+        for (int i=0; i<500; i++) {
+            payload.putInt(78789797);
+        }
+
+        payload.flip();
+
+        Entity entity = new Entity();
+        entity.setEtype(Type.ONE);
+        entity.setPayload(payload);
+
+        //serialize
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        ReflectDatumWriter<Entity> datumWriter = new ReflectDatumWriter<Entity>(Entity.class);
+        BinaryEncoder avroEncoder = EncoderFactory.get().blockingBinaryEncoder(outputStream, null);
+        datumWriter.write(entity, avroEncoder);
+        avroEncoder.flush();
+
+        byte[] bytes = outputStream.toByteArray();
+
+        //deserialize
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(bytes);
+        ReflectDatumReader<Entity> datumReader = new ReflectDatumReader<Entity>(Entity.class);
+        BinaryDecoder avroDecoder = DecoderFactory.get().binaryDecoder(inputStream, null);
+        Entity deserailized = datumReader.read(null, avroDecoder);
+
+        //check
         assertEquals(entity.getEtype(), deserailized.getEtype());
         assertEquals(entity.getPayload(), deserailized.getPayload());
     }
@@ -88,6 +132,7 @@ public class Tests {
         BinaryDecoder avroDecoder = DecoderFactory.get().binaryDecoder(inputStream, null);
         Entity deserailized = datumReader.read(null, avroDecoder);
 
+        //check
         assertEquals(entity.getEtype(), deserailized.getEtype());
         assertEquals(entity.getPayload(), deserailized.getPayload());
     }
